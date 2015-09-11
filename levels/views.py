46b61a2c999e6ec0b53776a283532aeb3c318dcc
Tonko Mulder <tonko@tonkomulder.nl>
2015-09-11 22:11:19 +0200

    added views for the levels app
    index, get_requirement and get_category
    
    Signed-off-by: Tonko Mulder <tonko@tonkomulder.nl>

--- levels/views.py
+++ levels/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.core.urlresolvers import reverse
+from .models import Requirement
 
-# Create your views here.
+
+def index(request):
+    return render(request, 'index.html', {
+        'cat_1': reverse('get_category', args=[1]),
+        'cat_2': reverse('get_category', args=[2]),
+        'cat_3': reverse('get_category', args=[3])
+        })
+
+
+def get_requirement(request, id):
+    items = Requirement.objects.select_related('category__name')
+    item = items.get(pk=id)
+    return render(request, 'get_requirement.html', {
+        'item': item,
+        'category': item.category,
+        'level': item.level_verbose()
+    })
+
+
+def get_category(request, id=None):
+    pass
