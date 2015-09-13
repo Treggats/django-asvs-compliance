d7ddb41f80947273f46020a9320a01996e48d0cc
Tonko Mulder <tonko@tonkomulder.nl>
2015-09-13 21:56:39 +0200

    renamed get_level to get_requirement and renamed the templates
    Signed-off-by: Tonko Mulder <tonko@tonkomulder.nl>

--- levels/views.py
+++ levels/views.py
@@ -5,21 +5,25 @@ from .models import Requirement
 
 def index(request):
     return render(request, 'index.html', {
-        'cat_1': reverse('get_category', args=[1]),
-        'cat_2': reverse('get_category', args=[2]),
-        'cat_3': reverse('get_category', args=[3])
+        'cat_1': reverse('get_level', args=[1]),
+        'cat_2': reverse('get_level', args=[2]),
+        'cat_3': reverse('get_level', args=[3])
         })
 
 
 def get_requirement(request, id):
     items = Requirement.objects.select_related('category__name')
     item = items.get(pk=id)
-    return render(request, 'get_requirement.html', {
+    return render(request, 'requirement_detail.html', {
         'item': item,
         'category': item.category,
         'level': item.level_verbose()
     })
 
 
-def get_category(request, id=None):
-    pass
+def get_level(request, id):
+    items = Requirement.objects.all()
+    if id in items.level_number():
+        return render(request, 'level_detail.html', {
+            'items': items.level_verbose()
+        })
