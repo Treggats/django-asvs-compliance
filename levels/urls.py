3eafb4abd3536be407e481b9b7fb3bc9847568c1
Tonko Mulder <tonko@tonkomulder.nl>
2015-09-14 13:52:50 +0200

    added category urlpatterns

--- levels/urls.py
+++ levels/urls.py
@@ -3,6 +3,9 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^requirement/(\d+)/$', views.get_requirement, name='get_requirement'),
+    url(r'^requirement/(\d+)/$', views.get_requirement,
+        name='get_requirement'),
     url(r'^level/(\d+)/$', views.get_level, name='get_level'),
+    url(r'^category/$', views.get_category, name='get_category_list'),
+    url(r'^category/(V[0-9]+)/$', views.get_category, name='get_category'),
 ]
