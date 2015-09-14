41e871d373300fe9440ed6514480db072c0e30ec
Tonko Mulder <tonko@tonkomulder.nl>
2015-09-14 10:15:45 +0200

    added localhost to ALLOWED_HOSTS
    would get a error 400 in production otherwise

--- asvs/settings.py
+++ asvs/settings.py
@@ -41,7 +41,7 @@ SECRET_KEY = str(SECRETS['secret_key'])
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
