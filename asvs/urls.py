1846bd640110944af1e5f3d4e30de086ac29fc43
Tonko Mulder <tonko@tonkomulder.nl>
2015-09-14 15:25:07 +0200

    let django serve the staticfiles

--- asvs/urls.py
+++ asvs/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'', include('levels.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
