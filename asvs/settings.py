653745a83899bd624f412ae9be120f58ca18624a
Tonko Mulder <tonko@tonkomulder.nl>
2015-09-15 10:53:51 +0200

    moved levels.css to app directory
    replaced STATICFILES_DIR with STATIC_ROOT

--- asvs/settings.py
+++ asvs/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [str(BASE_DIR.joinpath('static')), ]
+STATIC_ROOT = str(BASE_DIR.joinpath('static'))
 
 # Fixture files
 # https://docs.djangoproject.com/en/1.8/howto/initial-data/
