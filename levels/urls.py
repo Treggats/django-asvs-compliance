37c2590e73d0749a18a4054f19296b9949a36861
Tonko Mulder <mulder@goldmund-wyldebeast-wunderliebe.com>
2015-10-08 14:24:02 +0200

    removed root view
    Signed-off-by: Tonko Mulder <mulder@goldmund-wyldebeast-wunderliebe.com>

--- levels/urls.py
+++ levels/urls.py
@@ -2,7 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
     url(r'^requirement/(\d+)/$', views.get_requirement,
         name='get_requirement'),
     url(r'^level/(\d+)/$', views.get_level, name='get_level'),
