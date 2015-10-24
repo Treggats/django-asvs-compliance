b8197bc4b2fd9e8e5a260bbc72e91d8b8fce6f81
Tonko Mulder <tonko@tonkomulder.nl>
2015-10-25 00:41:43 +0200

    rewrote the script into a class, fully functional
    Signed-off-by: Tonko Mulder <tonko@tonkomulder.nl>

--- bin/create_fixture_from_csv.py
+++ bin/create_fixture_from_csv.py
@@ -6,35 +6,72 @@ import json
 
 
 class FixtureCreator(object):
+
     """
     FixtureCreator
 
-    Create a new instance with a csv input file
+    Open a csv file
     """
+
     def __init__(self, csv_file):
-        self.csv_file = csv.reader(csv_file,
-                                   delimiter=',',
-                                   quotechar='"')
+        self.DEBUG = False
+        self.csv_file = open(csv_file, encoding='utf-8')
+        self.csv = csv.reader(self.csv_file,
+                              delimiter=',',
+                              quotechar='"')
         self.categories = []
         self.requirements = []
         self.rows = []
         """Skip the first line of the csv
-        those are field markers
+        those are field markers.
+        TODO: there must be a better way to do this.
         """
-        self.reader = iter(self.csv_file)
+        self.reader = iter(self.csv)
         next(self.reader)
 
         self.csv_to_rows()
+        self.create_categories()
+        self.create_requirements()
 
-    """Put the contents of the csv in rows"""
+    def __del__(self):
+        """Close the csv file"""
+        self.csv_file.close()
+
+    def _replace_char(self, variable, char=None, new_char=None):
+        """Replace unicode character with something else"""
+        if char is None:
+            char = 10003
+        if new_char is None:
+            new_char = "Y"
+        if ord(variable) == char:
+            return new_char
+        return variable
+
     def csv_to_rows(self):
+        """Put the contents of the csv in rows"""
         for row in self.reader:
+            """Replace unicode character"""
+            if row[2]:
+                row[2] = self._replace_char(row[2])
+            if row[3]:
+                row[3] = self._replace_char(row[3])
+            if row[4]:
+                row[4] = self._replace_char(row[4])
             self.rows.append(row)
 
     def create_categories(self):
         for row in self.rows:
             if row[0].startswith("V") and "section" not in row[1]:
-                self.categories.append({row[0]: row[1]})
+                self.categories.append(dict(
+                                       version=row[0],
+                                       description=row[1]))
+        if self.DEBUG:
+            return json.dumps(self.categories, sort_keys=True, indent=4)
+
+    def _get_category_pk(self, version):
+        for k, x in enumerate(self.categories):
+            if x.get("version") == "V1":
+                return k+1
 
     def create_requirements(self):
         for row in self.rows:
@@ -45,86 +82,85 @@ class FixtureCreator(object):
                                          level_1=row[2],
                                          level_2=row[3],
                                          level_3=row[4]))
-        return json.dumps(self.requirements, sort_keys=True, indent=4)
+        if self.DEBUG:
+            return json.dumps(self.requirements, sort_keys=True, indent=4)
 
-with open("asvs_v3_xls.csv") as asvs_file:
-    fc = FixtureCreator(asvs_file)
-    cats = fc.create_categories()
-    # print(cats)
-    reqs = fc.create_requirements()
-    print(reqs)
-    asvs_file.close()
-
-
-"""
-def replace_character(var):
-    return var.decode("utf-8").replace(u"\u2713", "Y").encode("utf-8")
-
-
-def create_fixture(filename, data):
-    with open(filename, "w") as out_file:
-        json.dump(data, out_file,
-                  sort_keys=True,
-                  indent=4,
-                  separators=(',', ': '))
-
-with open("asvs_v3_xls.csv", "rb") as csv_file:
-    spamreader = csv.reader(csv_file, delimiter=",", quotechar='"')
-    sr = iter(spamreader)
-    next(sr)
-    categories = {}
-    requirements = []
-    for row in sr:
-        if row[0].startswith("V"):
-            if "section" not in row[1]:
-                categories[row[0]] = row[1]
-        else:
-            cat_id = row[0].split('.')[0]
-            row_id = row[0].split('.')[1]
-            category = categories.get("V{}".format(cat_id))
-            if DEBUG and int(cat_id) < 10:
-                cat_id = "0{}".format(cat_id)
-            if DEBUG and int(row_id) < 10:
-                row_id = "0{}".format(row_id)
-            requirements_format = "V{}.{}".format(cat_id, row_id)
-            requirements.append(dict(category_id=cat_id,
-                                category=category,
-                                requirement=row[1],
-                                req_id=requirements_format,
-                                level_1=replace_character(row[2]),
-                                level_2=replace_character(row[3]),
-                                level_3=replace_character(row[4])))
-
-    output = []
-    for pk, value in enumerate(categories.items()):
-        output.append({
-                      "fields": {
-                          "version": value[0],
-                          "name": value[1]
-                      },
-                      "model": "levels.category",
-                      "pk": pk + 1
-                      })
-    print(json.dumps(output, sort_keys=True, indent=4))
-
-    data = requirements
-    output = []
-    for pk, item in enumerate(data):
+    def generate_level_fixture(self):
         levels = []
-        if item["level_1"] == "Y":
-            levels.append(1)
-        if item["level_2"] == "Y":
-            levels.append(2)
-        if item["level_3"] == "Y":
-            levels.append(3)
-        output.append({
-                      "fields": {
-                          "category": item["category_id"],
-                          "description": item["requirement"],
-                          "number": levels
-                      },
-                      "model": "levels.requirement",
-                      "pk": pk + 1
-                      })
-    create_fixture("requirements.json", output)
-"""
+        levels.append(
+                      dict(
+                           fields=dict(
+                                       id=1,
+                                       description="Oppertunistic"),
+                           model="levels.levelnumber",
+                           pk=1))
+        levels.append(
+                      dict(
+                           fields=dict(
+                                       id=1,
+                                       description="Standard"),
+                           model="levels.levelnumber",
+                           pk=2))
+        levels.append(
+                      dict(
+                           fields=dict(
+                                       id=1,
+                                       description="Advanced"),
+                           model="levels.levelnumber",
+                           pk=3))
+        return json.dumps(levels,
+                          sort_keys=True,
+                          indent=4,
+                          separators=(',', ': '))
+
+    def generate_category_fixture(self):
+        output = []
+        for pk, item in enumerate(self.categories):
+            output.append(
+                          dict(
+                               fields=dict(
+                                           version=item.get("version"),
+                                           name=item.get("description")),
+                               model="levels.category",
+                               pk=pk+1))
+        return json.dumps(output,
+                          sort_keys=True,
+                          indent=4,
+                          separators=(',', ': '))
+
+    def generate_requirement_fixture(self):
+        output = []
+        for pk, item in enumerate(self.requirements):
+            levels = []
+            id = "V{}".format(item["id"].split('.')[0])
+            if item["level_1"] == "Y":
+                levels.append(1)
+            if item["level_2"] == "Y":
+                levels.append(2)
+            if item["level_3"] == "Y":
+                levels.append(3)
+            output.append(
+                          dict(
+                               fields=dict(
+                                           category=self._get_category_pk(id),
+                                           description=item["description"],
+                                           number=levels),
+                               model="levels.requirement",
+                               pk=pk + 1))
+        return json.dumps(output,
+                          sort_keys=True,
+                          indent=4,
+                          separators=(',', ': '))
+
+asvs_file = "asvs_v3_xls.csv"
+fc = FixtureCreator(asvs_file)
+"""Use a write operation to create files"""
+with open("levels.json", "w") as outfile:
+    lvl_fixt = fc.generate_level_fixture()
+    outfile.write(lvl_fixt)
+with open("categories.json", "w") as outfile:
+    cat_fixt = fc.generate_category_fixture()
+    outfile.write(cat_fixt)
+with open("requirements.json", "w") as outfile:
+    req_fixt = fc.generate_requirement_fixture()
+    outfile.write(req_fixt)
