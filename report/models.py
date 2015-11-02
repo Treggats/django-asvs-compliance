58aaac63300ace5d67bdf2bf91acb8f43477097e
Tonko Mulder <tonko@tonkomulder.nl>
2015-11-03 00:57:11 +0100

    removed report as a app
    Signed-off-by: Tonko Mulder <tonko@tonkomulder.nl>

--- report/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-from project.models import Project
-from level.models import Requirement
-
-
-class Report(models.Model):
-    project = models.ForeignKey(Project)
-    requirements = models.ForeignKey(Requirement)
