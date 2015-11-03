4650ebe93f07d1b75cce7e43c99ba2743d90badc
Tonko Mulder <mulder@goldmund-wyldebeast-wunderliebe.com>
2015-11-03 10:03:03 +0100

    added python2 support
    Signed-off-by: Tonko Mulder <mulder@goldmund-wyldebeast-wunderliebe.com>

--- project/models.py
+++ project/models.py
@@ -1,15 +1,25 @@
+from __future__ import unicode_literals
+from django.utils.encoding import python_2_unicode_compatible
 from django.db import models
 from level.models import LevelNumber, Requirement
 
 
+@python_2_unicode_compatible
 class Project(models.Model):
     name = models.CharField(max_length=40)
     description = models.TextField()
     level = models.ForeignKey(LevelNumber)
     done = models.BooleanField()
 
+    def __str__(self):
+        return self.name
 
+
+@python_2_unicode_compatible
 class Report(models.Model):
     project = models.ForeignKey(Project)
     requirements = models.ForeignKey(Requirement)
     signed = models.TextField()
+
+    def __str__(self):
+        return self.name
