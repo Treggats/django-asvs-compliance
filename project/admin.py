304be9d751e539b1f92f53efc0663714fd8a10cd
Tonko Mulder <mulder@goldmund-wyldebeast-wunderliebe.com>
2015-11-03 10:24:06 +0100

    added ProjectAdmin and ReportAdmin
    Signed-off-by: Tonko Mulder <mulder@goldmund-wyldebeast-wunderliebe.com>

--- project/admin.py
+++ project/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
 from .models import Project, Report
 
-admin.site.register(Project)
-admin.site.register(Report)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('name', 'level', 'done')
+
+
+class ReportAdmin(admin.ModelAdmin):
+    list_filter = ['project']
+
+
+admin.site.register(Project, ProjectAdmin)
+admin.site.register(Report, ReportAdmin)
