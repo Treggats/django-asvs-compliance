b2afeee05342e799391051202f9857367d825210
Tonko Mulder <tonko@tonkomulder.nl>
2015-11-03 01:14:03 +0100

    put project and report in project app
    Signed-off-by: Tonko Mulder <tonko@tonkomulder.nl>

--- project/models.py
+++ project/models.py
@@ -1,12 +1,15 @@
 from django.db import models
-from level.models import LevelNumber
-from report.models import Report
+from level.models import LevelNumber, Requirement
 
 
 class Project(models.Model):
-    name = models.CharField()
+    name = models.CharField(max_length=40)
     description = models.TextField()
     level = models.ForeignKey(LevelNumber)
-    report = models.ForeignKey(Report)
     done = models.BooleanField()
+
+
+class Report(models.Model):
+    project = models.ForeignKey(Project)
+    requirements = models.ForeignKey(Requirement)
     signed = models.TextField()
