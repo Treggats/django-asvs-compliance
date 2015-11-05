ddd48f1657abdf15ec39379b22b38e12fa02e160
Tonko Mulder <tonko@tonkomulder.nl>
2015-11-05 23:50:54 +0100

    updated level management command
    it now listens to argument --type
    which will need to provide either 'asvs' or 'aasvs'
    and executing each individual class
    
    Signed-off-by: Tonko Mulder <tonko@tonkomulder.nl>

--- level/management/commands/asvsmakefixture.py
+++ level/management/commands/asvsmakefixture.py
@@ -1,31 +1,38 @@
-from django.core.management.base import BaseCommand
-
+from django.core.management.base import BaseCommand, CommandError
 import os
-from pathlib import Path
-
-
 
 from level.management.helpers.asvs import ASVS
 from level.management.helpers.aasvs import AASVS
+
 
 class Command(BaseCommand):
     """
     """
 
-    def handle(self, *args, **options):
-        print('Baaa')
+    def __init__(self):
+        super().__init__()
+        self.cls = None
 
-        data_dir = 'django_asvs_compliance'
-        try:
-            project_path = Path(os.environ[data_dir])
-            asvs_file = "{}/bin/helpers/asvs.json".format(project_path)
-            aasvs_file = "{}/bin/helpers/aasvs.json".format(project_path)
-            asvs = ASVS(asvs_file)
-            aasvs = AASVS(aasvs_file)
-            print(aasvs)
-            # fc = FixtureCreator(asvs_file)
-            # fixture = fc.annotated_fixture()
-            # print(fixture)
-            # import ipdb; ipdb.set_trace()
-        except KeyError:
-            print("Data dir '{}' has not been set.".format(data_dir))
+    help = "Create fixtures to load ASVS data into the models"
+
+    def add_arguments(self, parser):
+        parser.add_argument('--type', nargs='+', type=str)
+
+    def handle(self, **options):
+        json_path = os.path.realpath(__file__)
+        commands_path = os.path.abspath(os.path.join(json_path, os.pardir))
+        management_path = os.path.abspath(os.path.join(commands_path, os.pardir))
+        if not options['type']:
+            self.stdout.write("Argument --type is not provided.")
+        else:
+            for type in options['type']:
+                try:
+                    if type == 'asvs':
+                        asvs_file = "{}/helpers/asvs.json".format(management_path)
+                        self.cls = ASVS(asvs_file)
+                    elif type == 'aasvs':
+                        aasvs_file = "{}/helpers/aasvs.json".format(management_path)
+                        self.cls = AASVS(aasvs_file)
+                    print(self.cls)
+                except:
+                    raise CommandError("Type: {} is not valid".format(type))
