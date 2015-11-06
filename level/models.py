c32b94657b80a788cfc47544784fdf920b870bdc
Tonko Mulder <mulder@goldmund-wyldebeast-wunderliebe.com>
2015-11-06 10:42:44 +0100

    [issue #3] added an identifier to the models
    reindented level/models.py
    
    Signed-off-by: Tonko Mulder <mulder@goldmund-wyldebeast-wunderliebe.com>

--- level/models.py
+++ level/models.py
@@ -5,68 +5,71 @@ from django.db import models
 
 @python_2_unicode_compatible
 class AsvsVersion(models.Model):
-          version_number = models.CharField(max_length=10, default='3')
+      version_number = models.CharField(max_length=10, default='3')
 
-          def __str__(self):
-                    return self.version_number
+      def __str__(self):
+          return self.version_number
 
 
 @python_2_unicode_compatible
 class LevelName(models.Model):
-          lang_code = models.CharField(max_length=5)
-          name = models.CharField(max_length=100)
+    level_number = models.PositiveIntegerField()
+    lang_code = models.CharField(max_length=5)
+    name = models.CharField(max_length=100)
 
-          def __str__(self):
-                    return self.name
+    def __str__(self):
+        return self.name
 
 
 @python_2_unicode_compatible
 class Level(models.Model):
-          number = models.PositiveSmallIntegerField()
-          name = models.ForeignKey(LevelName)
-          version = models.ForeignKey(AsvsVersion, default='3')
+      number = models.PositiveSmallIntegerField()
+      name = models.ForeignKey(LevelName)
+      version = models.ForeignKey(AsvsVersion, default='3')
 
-          def __str__(self):
-                    return "{0}: {1}".format(self.number, self.name)
+      def __str__(self):
+          return "{0}: {1}".format(self.number, self.name)
 
 
 @python_2_unicode_compatible
 class CategoryName(models.Model):
-          lang_code = models.CharField(max_length=5)
-          name = models.CharField(max_length=100)
+    category_number = models.PositiveIntegerField()
+    lang_code = models.CharField(max_length=5)
+    name = models.CharField(max_length=100)
 
-          def __str__(self):
-                    return self.name
+    def __str__(self):
+        return self.name
 
 
 @python_2_unicode_compatible
 class Category(models.Model):
-          number = models.PositiveSmallIntegerField()
-          name = models.ForeignKey(CategoryName)
-          version = models.ForeignKey(AsvsVersion, default='3')
+      number = models.PositiveSmallIntegerField()
+      name = models.ForeignKey(CategoryName)
+      version = models.ForeignKey(AsvsVersion, default='3')
 
-          class Meta:
-                    verbose_name_plural = 'Categories'
+      class Meta:
+          verbose_name_plural = 'Categories'
 
-          def __str__(self):
-                    return "{0}: {1}".format(self.number, self.name)
+      def __str__(self):
+          return "{0}: {1}".format(self.number, self.name)
 
 
 @python_2_unicode_compatible
 class RequirementName(models.Model):
-          lang_code = models.CharField(max_length=5)
-          title = models.TextField()
+    requirement_number = models.PositiveIntegerField()
+    lang_code = models.CharField(max_length=5)
+    title = models.TextField()
 
-          def __str__(self):
-                    return "{}".format(self.title)
+    def __str__(self):
+        return "{}".format(self.title)
 
 
 @python_2_unicode_compatible
 class Requirement(models.Model):
-          number = models.PositiveSmallIntegerField()
-          category = models.ForeignKey(Category)
-          name = models.ForeignKey(RequirementName)
-          version = models.ForeignKey(AsvsVersion, default='3')
+      number = models.PositiveSmallIntegerField()
+      category = models.ForeignKey(Category)
+      name = models.ForeignKey(RequirementName)
+      version = models.ForeignKey(AsvsVersion, default='3')
 
-          def __str__(self):
-                    return "{}".format(self.name)
+      def __str__(self):
+          return "{}".format(self.name)
