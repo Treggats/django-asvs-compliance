23457e9aba76a1a518a653ea22c7cf6bead9eadd
Tonko Mulder <mulder@goldmund-wyldebeast-wunderliebe.com>
2015-11-06 17:00:59 +0100

    updated level/admin.py with list_display and ordering
    for every level admin
    
    Signed-off-by: Tonko Mulder <mulder@goldmund-wyldebeast-wunderliebe.com>

--- level/admin.py
+++ level/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.contrib.admin import models
+
 from .models import AsvsVersion
 from .models import Level, LevelName
 from .models import Category, CategoryName
@@ -7,34 +9,40 @@ from .models import Requirement, RequirementName
 
 @admin.register(AsvsVersion)
 class AsvsVersionAdmin(admin.ModelAdmin):
-    pass
+    ordering = ('version_number',)
 
 
 @admin.register(Level)
 class LevelAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('number', 'name', 'version')
+    ordering = ('number',)
 
 
 @admin.register(LevelName)
 class LevelNameAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('level_number', 'name', 'lang_code')
+    ordering = ('level_number',)
 
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('number', 'name', 'version')
+    ordering = ('number',)
 
 
 @admin.register(CategoryName)
 class CategoryNameAdmin(admin.ModelAdmin):
-    list_display = ('category_number', 'lang_code', 'name')
+    list_display = ('category_number', 'name', 'lang_code')
+    ordering = ('category_number',)
 
 
 @admin.register(Requirement)
 class RequirementAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('number', 'name', 'category', 'version')
+    ordering = ('number',)
 
 
 @admin.register(RequirementName)
 class RequirementNameAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('requirement_number', 'title', 'lang_code')
+    ordering = ('requirement_number',)
