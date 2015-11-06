ea7a0603820a71893d5c06434f4f8140d50418c1
Tonko Mulder <mulder@goldmund-wyldebeast-wunderliebe.com>
2015-11-06 10:16:15 +0100

    added --type values
    show the types in the message
    
    Signed-off-by: Tonko Mulder <mulder@goldmund-wyldebeast-wunderliebe.com>

--- level/management/commands/asvsmakefixture.py
+++ level/management/commands/asvsmakefixture.py
@@ -12,6 +12,7 @@ class Command(BaseCommand):
     def __init__(self):
         super().__init__()
         self.cls = None
+        self.type_values = ('asvs', 'aasvs')
 
     help = "Create fixtures to load ASVS data into the models"
 
@@ -23,7 +24,8 @@ class Command(BaseCommand):
         commands_path = os.path.abspath(os.path.join(json_path, os.pardir))
         management_path = os.path.abspath(os.path.join(commands_path, os.pardir))
         if not options['type']:
-            self.stdout.write("Argument --type is not provided.")
+            self.stdout.write("Argument --type ["+', '.join(
+                self.type_values)+"] is not provided.")
         else:
             for type in options['type']:
                 try:
@@ -33,6 +35,9 @@ class Command(BaseCommand):
                     elif type == 'aasvs':
                         aasvs_file = "{}/helpers/aasvs.json".format(management_path)
                         self.cls = AASVS(aasvs_file)
+                    else:
+                        self.cls = "The type values are: " + ", ".join(
+                            self.type_values)
                     print(self.cls)
                 except:
                     raise CommandError("Type: {} is not valid".format(type))
