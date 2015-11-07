895e5cf494c18ac207b5d2b563954bb1e06ad3e1
Tonko Mulder <mulder@goldmund-wyldebeast-wunderliebe.com>
2015-11-07 12:59:33 +0100

    updated __init__ with super call
    reformated code
    
    Signed-off-by: Tonko Mulder <mulder@goldmund-wyldebeast-wunderliebe.com>

--- level/management/commands/asvsmakefixture.py
+++ level/management/commands/asvsmakefixture.py
@@ -1,7 +1,6 @@
 # -*- encoding: utf-8 -*-
 from django.core.management.base import BaseCommand, CommandError
 import os
-
 from level.management.helpers.asvs import ASVS
 from level.management.helpers.aasvs import AASVS
 
@@ -10,8 +9,8 @@ class Command(BaseCommand):
     """
     """
 
-    def __init__(self):
-        super().__init__()
+    def __init__(self, *args, **kwargs):
+        super(Command, self).__init__(*args, **kwargs)
         self.cls = None
         self.type_values = ('asvs', 'aasvs')
 
@@ -23,18 +22,21 @@ class Command(BaseCommand):
     def handle(self, **options):
         json_path = os.path.realpath(__file__)
         commands_path = os.path.abspath(os.path.join(json_path, os.pardir))
-        management_path = os.path.abspath(os.path.join(commands_path, os.pardir))
+        management_path = os.path.abspath(
+            os.path.join(commands_path, os.pardir))
         if not options['type']:
-            self.stdout.write("Argument --type ["+', '.join(
-                self.type_values)+"] is not provided.")
+            self.stdout.write("Argument --type [" + ', '.join(
+                self.type_values) + "] is not provided.")
         else:
             for type in options['type']:
                 try:
                     if type == 'asvs':
-                        asvs_file = "{}/helpers/asvs.json".format(management_path)
+                        asvs_file = "{}/helpers/asvs.json".format(
+                            management_path)
                         self.cls = ASVS(asvs_file)
                     elif type == 'aasvs':
-                        aasvs_file = "{}/helpers/aasvs.json".format(management_path)
+                        aasvs_file = "{}/helpers/aasvs.json".format(
+                            management_path)
                         self.cls = AASVS(aasvs_file)
                     else:
                         self.cls = "The type values are: " + ", ".join(
