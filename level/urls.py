14c431893e01c4a83cd1e1eb7232fb80fc5410eb
Tonko Mulder <mulder@goldmund-wyldebeast-wunderliebe.com>
2015-11-24 13:51:24 +0100

    updated templates to work with the skeleton framework
    Signed-off-by: Tonko Mulder <mulder@goldmund-wyldebeast-wunderliebe.com>

--- level/urls.py
+++ level/urls.py
@@ -2,9 +2,11 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url(r'^$', views.index, name='home'),
+    url(r'^requirement/$', views.get_requirement, name='requirement_list'),
     url(r'^requirement/(\d+)/$', views.get_requirement,
-        name='get_requirement'),
+        name='requirement_detail'),
     url(r'^level/(\d+)/$', views.get_level, name='get_level'),
     url(r'^category/$', views.get_category, name='get_category_list'),
-    url(r'^category/(V[0-9]+)/$', views.get_category, name='get_category'),
+    url(r'^category/([0-9]+)/$', views.get_category, name='get_category'),
 ]
