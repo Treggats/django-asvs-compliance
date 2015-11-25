17606632d4382275a05c2b42b6f7c209c423937c
Tonko Mulder <tonko@tonkomulder.nl>
2015-11-25 11:55:16 +0100

    updated AASVS class to skip annotations without a corrensponding requirement
    Signed-off-by: Tonko Mulder <tonko@tonkomulder.nl>

--- level/management/helpers/aasvs.py
+++ level/management/helpers/aasvs.py
@@ -15,53 +15,40 @@ class AASVS(object):
         """Close the csv file"""
         self.json_file.close()
 
-    def get_requirement_by_number(self, req_nr, cat_nr):
-        nr = 1
-        requirement = Requirement.objects.language()\
-            .filter(category__category_number=cat_nr)
-        for req in requirement:
-            if int(req_nr) == nr:
-                return req
-            nr += 1
-
     def load_requirement(self):
         for pk, value in enumerate(self.reader.get('requirements'), start=1):
             lang_code = list(value.get('shortTitle'))[0]
             req_nr = value.get('nr')
             cat_nr = value.get('chapterNr')
             title = value.get('shortTitle').get(lang_code)
-            print("Req:  {}\n"
-                  "Cat: {}\n"
-                  "Title: {}".format(req_nr, cat_nr, title))
+
             req = Requirement.objects.language().filter(
                 category__category_number=cat_nr).filter(
                 requirement_number=req_nr)
-            print(req)
-            print()
+            if not req:
+                req = Requirement.DoesNotExist
+                print("Requirement {} does not exist.".format(req_nr))
 
-            # for item in value.get('related'):
-            #     related = RelatedAnnotated.objects.language(lang_code).create(
-            #         name=item.get('name'),
-            #         url=item.get('url')
-            #     )
-            #     related.save()
+            for item in value.get('related'):
+                related = RelatedAnnotated.objects.language(lang_code).create(
+                    name=item.get('name'),
+                    url=item.get('url')
+                )
+                related.save()
 
-            # r = Requirement.objects.language().filter(
-            #     category__category_number=cat_nr).get(
-            #     requirement_number=int(value.get('nr')))
-            #
-            # requirement = RequirementAnnotated.objects.language(lang_code)\
-            #     .get_or_create(
-            #         pk=pk,
-            #         requirement=r,
-            #         category=Category.objects.language(lang_code).get(
-            #             category_number=cat_nr),
-            #         title=title
-            # )
-            # requirement.save()
+            if req is not Requirement.DoesNotExist:
+                requirement = RequirementAnnotated.objects.language(lang_code)\
+                    .create(
+                        pk=pk,
+                        requirement=req[0],
+                        category=Category.objects.language(lang_code).get(
+                            category_number=cat_nr),
+                        title=title
+                )
+                requirement.save()
 
-            # for item in value.get('related'):
-            #     related_items = RelatedAnnotated.objects.language(
-            #         lang_code).filter(name__exact=item.get('name'))
-            #     requirement.related = related_items
-            #     requirement.save()
+            for item in value.get('related'):
+                related_items = RelatedAnnotated.objects.language(
+                    lang_code).filter(name__exact=item.get('name'))
+                requirement.related = related_items
+                requirement.save()
