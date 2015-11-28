5f34557b2418f6ed25e5b11aa7b8fb47c4965a8f
Tonko Mulder <tonko@tonkomulder.nl>
2015-11-28 21:13:39 +0100

    fixed the import error
    Signed-off-by: Tonko Mulder <tonko@tonkomulder.nl>

--- level/templatetags/markdown_markup.py
+++ level/templatetags/markdown_markup.py
@@ -3,17 +3,18 @@ import markdown
 
 from django import template
 from django.template.defaultfilters import stringfilter
-# from django.utils.encoding import force_unicode
+from django.utils.encoding import force_text
 from django.utils.safestring import mark_safe
 
 register = template.Library()
+
 
 @register.filter(is_safe=True)
 @stringfilter
 def md(value):
     extensions = ["nl2br", ]
 
-    return mark_safe(markdown.markdown(value,
+    return mark_safe(markdown.markdown(force_text(value),
                                        extensions,
                                        safe_mode=True,
                                        enable_attributes=False))
