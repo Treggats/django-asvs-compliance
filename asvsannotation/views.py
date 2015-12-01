9061b96758a8645a499f922f9ec7f7859911e516
Tonko Mulder <mulder@goldmund-wyldebeast-wunderliebe.com>
2015-12-01 11:27:41 +0100

    asvsannotation.views is currently being done by asvsrequirement
    Signed-off-by: Tonko Mulder <mulder@goldmund-wyldebeast-wunderliebe.com>

--- asvsannotation/views.py
+++ asvsannotation/views.py
@@ -1,17 +1,1 @@
 from django.shortcuts import render
-from asvs.settings import LANGUAGE_CODE
-from asvsannotation.models import AnnotationExplanation, AnnotationRequirement
-
-
-def get_explanation(request, id=None):
-    if id is None:
-        items = AnnotationExplanation.objects.all()
-        return render(request, 'annotation_list.html', {
-            'items': items
-        })
-    else:
-        explanation = AnnotationExplanation.objects.get(pk=id)
-        return render(request, 'annotation_explanation.html', {
-            'req': explanation.req_ann,
-            'info': explanation,
-        })
