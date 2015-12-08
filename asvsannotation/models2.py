28a3cab23f21ae3dc30a8e6b9ca351017b013f1f
Tonko Mulder <mulder@goldmund-wyldebeast-wunderliebe.com>
2015-12-08 15:29:37 +0100

    added possible refactor of asvsannotation.models
    Signed-off-by: Tonko Mulder <mulder@goldmund-wyldebeast-wunderliebe.com>

---
+++ asvsannotation/models2.py
@@ -0,0 +1,70 @@
+from __future__ import unicode_literals
+
+from django.utils.encoding import python_2_unicode_compatible
+from hvad.models import TranslatableModel, TranslatedFields
+from django.db import models
+from asvsrequirement.models import Requirement, Category
+
+
+@python_2_unicode_compatible
+class AnnotationType(TranslatableModel):
+    translations = TranslatedFields(
+        title=models.CharField(max_length=40)
+    )
+
+
+@python_2_unicode_compatible
+class AnnotationHelp(TranslatableModel):
+    requirement = models.ForeignKey(Requirement)
+    category = models.ForeignKey(Category)
+    annotation_type = models.ForeignKey(AnnotationType)
+
+    translations = TranslatedFields(
+        help_text=models.TextField()
+    )
+
+    class Meta:
+        unique_together = ('requirement', 'category')
+
+
+@python_2_unicode_compatible
+class AnnotationRelation(TranslatableModel):
+    requirement = models.ForeignKey(Requirement)
+    category = models.ForeignKey(Category)
+    url = models.URLField()
+
+    translations = TranslatedFields(
+        relation_title=models.CharField(max_length=100)
+    )
+
+    @property
+    def title_(self):
+        return self.relation_title
+
+    class Meta:
+        unique_together = ('requirement', 'category')
+
+    def __str__(self):
+        return self.title_
+
+
+@python_2_unicode_compatible
+class Annotation(TranslatableModel):
+    requirement = models.ForeignKey(Requirement)
+    category = models.ForeignKey(Category)
+    annotation_help = models.ForeignKey(AnnotationHelp, blank=True, null=True)
+    relations = models.ManyToManyField(AnnotationRelation)
+
+    translations = TranslatedFields(
+        title=models.CharField(max_length=100)
+    )
+
+    @property
+    def title_(self):
+        return self.title
+
+    class Meta:
+        unique_together = ('requirement', 'category')
+
+    def __str__(self):
+        return self.title_
