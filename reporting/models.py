46ceab1bfb81697df4bbf9e4bade5ed9f2be8b95
Tonko Mulder <mulder@goldmund-wyldebeast-wunderliebe.com>
2015-12-29 10:27:58 +0100

    added a default value for release_date
    Signed-off-by: Tonko Mulder <mulder@goldmund-wyldebeast-wunderliebe.com>

--- reporting/models.py
+++ reporting/models.py
@@ -1,6 +1,7 @@
 from __future__ import unicode_literals
 from django.utils.encoding import python_2_unicode_compatible
 from django.db import models
+from django.utils import timezone
 
 from asvsrequirement.models import Level, Requirement
 
@@ -31,7 +32,7 @@ class Project(models.Model):
 @python_2_unicode_compatible
 class Report(models.Model):
     release_name = models.CharField(max_length=40)
-    release_date = models.DateField()
+    release_date = models.DateField(default=timezone.now)
     generation_date = models.DateField(auto_now_add=True)
     project = models.ForeignKey(Project)
 
